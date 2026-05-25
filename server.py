ba5a5663353355eed930d4f726c956a5544abfab
mcristinapatron-cyber <m_cristina_patron@yahoo.com>
2026-05-25 02:49:34 -0400

    Update Task6

---
+++ server.py
@@ -0,0 +1,50 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask package
+from flask import Flask, render_template, request
+
+# Import the emotion_detector function from the package created
+from EmotionDetection.emotion_detection import emotion_detector
+
+#Initiate the flask app
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detection()
+        function. The output returned shows the emotions, including dominant_emotion
+        and the corresponding scores for the provided text.
+    '''
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Parse the response into given output format
+    emotions = list(response.keys())
+    emotions.remove('dominant_emotion')
+
+    response_formatted = "For the given statement, the system response is "
+    for emotion in emotions:
+        response_formatted += f"{emotion}: {response[emotion]}, "
+    response_formatted = response_formatted[:-2] + (
+        f". The dominant emotion is {response['dominant_emotion']}."
+    )
+
+    return response_formatted
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    # Return the render template
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0",port=5000)
